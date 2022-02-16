bf43cbc6389e1d643b804d09862b548e4d5f77b8
Jaume Nogues <jnogues@gmail.com>
2022-02-16 05:58:03 +0100

    Create 12_2022_01_04_ex12-OTA-telecos.ino

---
+++ code/12_2022_01_04_ex12-OTA-telecos.ino
@@ -0,0 +1,101 @@
+//_2022_01_04_ex12-OTA-telecos.ino
+//curs tardor 2022 telecos.cat
+//jnogues@gmail.com
+//Exemple bàsic per OverTheAir flashing
+
+#include <Blynk.h>
+BlynkTimer taskScheduler;//organitzador de tasques
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#ifndef STASSID
+#define STASSID "xxxxxxxxxxxxx"
+#define STAPSK  "xxxxxxxxxxxxx"
+#define STAHOSTNAME "elMeu1rOTA"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+#define led_blau 16  //led blau de la nodeMCU 
+
+void setup() {
+  pinMode(led_blau, OUTPUT);
+  digitalWrite(led_blau, HIGH); 
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(STAHOSTNAME);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  taskScheduler.setInterval(200L,  toggleLed); //tasca 1
+}
+
+void loop() 
+{
+  ArduinoOTA.handle();
+  taskScheduler.run();
+}
+
+
+//***********************TASQUES***********************
+
+//tasca 1
+void toggleLed()
+{
+  digitalWrite(led_blau, !digitalRead(led_blau));
+}
